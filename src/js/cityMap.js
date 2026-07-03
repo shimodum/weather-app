4c193167ca0f0bb0521350c4b98db5f905195d38
akihiro shimoduma <a.shimodum@gmail.com>
2026-07-04 05:22:54 +0900

    feat: 都道府県名検索対応とREADME更新

--- src/js/cityMap.js
+++ src/js/cityMap.js
@@ -1,4 +1,5 @@
 export const cityMap = {
+  // ===== 都市名 =====
   '札幌': 'Sapporo',
   '青森': 'Aomori',
   '盛岡': 'Morioka',
@@ -46,5 +47,106 @@ export const cityMap = {
   '大分': 'Oita',
   '宮崎': 'Miyazaki',
   '鹿児島': 'Kagoshima',
-  '那覇': 'Naha'
+  '那覇': 'Naha',
+
+  // ===== 都道府県名 =====
+  '北海道': 'Sapporo',
+
+  '青森県': 'Aomori',
+  '岩手': 'Morioka',
+  '岩手県': 'Morioka',
+  '宮城': 'Sendai',
+  '宮城県': 'Sendai',
+  '秋田県': 'Akita',
+  '山形県': 'Yamagata',
+  '福島県': 'Fukushima',
+
+  '茨城': 'Mito',
+  '茨城県': 'Mito',
+  '栃木': 'Utsunomiya',
+  '栃木県': 'Utsunomiya',
+  '群馬': 'Maebashi',
+  '群馬県': 'Maebashi',
+  '埼玉': 'Saitama',
+  '埼玉県': 'Saitama',
+  '千葉県': 'Chiba',
+
+  '東京都': 'Tokyo',
+
+  '神奈川': 'Yokohama',
+  '神奈川県': 'Yokohama',
+
+  '新潟県': 'Niigata',
+  '富山県': 'Toyama',
+
+  '石川': 'Kanazawa',
+  '石川県': 'Kanazawa',
+
+  '福井県': 'Fukui',
+
+  '山梨県': 'Kofu',
+
+  '長野県': 'Nagano',
+
+  '岐阜県': 'Gifu',
+
+  '静岡県': 'Shizuoka',
+
+  '愛知': 'Nagoya',
+  '愛知県': 'Nagoya',
+
+  '三重': 'Tsu',
+  '三重県': 'Tsu',
+
+  '滋賀': 'Otsu',
+  '滋賀県': 'Otsu',
+
+  '京都府': 'Kyoto',
+
+  '大阪府': 'Osaka',
+
+  '兵庫': 'Kobe',
+  '兵庫県': 'Kobe',
+
+  '奈良県': 'Nara',
+
+  '和歌山県': 'Wakayama',
+
+  '鳥取県': 'Tottori',
+
+  '島根': 'Matsue',
+  '島根県': 'Matsue',
+
+  '岡山県': 'Okayama',
+
+  '広島県': 'Hiroshima',
+
+  '山口県': 'Yamaguchi',
+
+  '徳島県': 'Tokushima',
+
+  '香川': 'Takamatsu',
+  '香川県': 'Takamatsu',
+
+  '愛媛': 'Matsuyama',
+  '愛媛県': 'Matsuyama',
+
+  '高知県': 'Kochi',
+
+  '福岡県': 'Fukuoka',
+
+  '佐賀県': 'Saga',
+
+  '長崎県': 'Nagasaki',
+
+  '熊本県': 'Kumamoto',
+
+  '大分県': 'Oita',
+
+  '宮崎県': 'Miyazaki',
+
+  '鹿児島県': 'Kagoshima',
+
+  '沖縄': 'Naha',
+  '沖縄県': 'Naha'
 };
